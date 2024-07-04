e1e1fa4a6f5fb33f11978595fdf044260327f0bd
Lenonkun <lenonkun10@gmail.com>
2024-07-04 09:04:56 +0300

    Реализация удалённого запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.bouncycastle.oer.its.etsi102941.Url;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,10 +9,13 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -36,17 +40,23 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
+        if ("".equals(properties.getProperty("selenium.server"))) {
 
-        if (Objects.equals(browser, Browser.CHROME.browserName())) {
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\IdeaProjects\\chromedriver-win64\\chromedriver.exe");
-            wd = new ChromeDriver();
-        } else if (Objects.equals(browser, Browser.EDGE.browserName())) {
+            if (Objects.equals(browser, Browser.CHROME.browserName())) {
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\IdeaProjects\\chromedriver-win64\\chromedriver.exe");
+                wd = new ChromeDriver();
+            } else if (Objects.equals(browser, Browser.EDGE.browserName())) {
 //            System.setProperty("webdriver.edge.driver", "C:\\Users\\user\\IdeaProjects\\edgedriver_win64\\msedgedriver.exe");
-            WebDriverManager.edgedriver().setup();
-            wd = new EdgeDriver();
-        } else if (Objects.equals(browser, Browser.IE.browserName())) {
-            WebDriverManager.iedriver().setup();
-            wd = new InternetExplorerDriver();
+                WebDriverManager.edgedriver().setup();
+                wd = new EdgeDriver();
+            } else if (Objects.equals(browser, Browser.IE.browserName())) {
+                WebDriverManager.iedriver().setup();
+                wd = new InternetExplorerDriver();
+            }
+        }else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("seleniup.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.get(properties.getProperty("web.baseUrl"));
