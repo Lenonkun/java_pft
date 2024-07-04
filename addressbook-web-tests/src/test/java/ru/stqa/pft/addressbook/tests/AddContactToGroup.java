8b78773644a1df340b8b524e64e9102375dcdbfd
Lenonkun <lenonkun10@gmail.com>
2024-07-04 11:13:00 +0300

    исправлена реализация тестов довавления котрактов в группы удаления из группы контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroup.java
@@ -1,20 +1,37 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
 import java.util.Set;
 
 @Test
-public class AddContactToGroup extends TestBase{
+public class AddContactToGroup extends TestBase {
+    @BeforeMethod
+    public void ensurePreconditions() {
+        app.contact().goToHomePage();
+        if (app.db().contacts().size() == 0) {
+            app.contact().create(new ContactData()
+                    .withFname("ivan4").withMname("ivanovich").withLname("ivanov").withAddress("123")
+                    .withMobile("999").withEmail("123@ya.ru").withBday("11").withBmonth("January")
+                    .withByear("1990").withAddress2("123123"), true);
+        }
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("group1"));
+        }
+
+    }
 
     @Test
     public void testAddContactToGroup() {
         Contacts beforeContacts = app.db().contacts();
-        Groups beforeGroups= app.db().groups();
+        Groups beforeGroups = app.db().groups();
         ContactData selectContact = beforeContacts.iterator().next();
         Set<GroupData> contactGroups = selectContact.getGroups();
         GroupData group = beforeGroups.stream().filter(g -> !contactGroups.contains(g))
@@ -29,15 +46,15 @@ public class AddContactToGroup extends TestBase{
                     .findFirst().orElse(null);//выбрать новую группу
         }
 
-        app.contact().addToGroup(selectContact,group.getId());
+        app.contact().addToGroup(selectContact, group.getId());
         app.contact().goToHomePage();
 
         Groups afterGroups = app.db().groups();
         //final - вынужденная мера для лямбда
         final GroupData fGroup = group;
-        GroupData addedGroup = afterGroups.stream().filter(g -> g.getId()== fGroup.getId())
+        GroupData addedGroup = afterGroups.stream().filter(g -> g.getId() == fGroup.getId())
                 .findFirst().orElse(null);//найти группу для проверок ниже
-        
+
         Set<ContactData> contactsInGroup = addedGroup.getContacts();
 //        System.out.println("Group "+addedGroup);
 //        System.out.println("should be a"+selectContact);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroup.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -11,6 +12,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class RemoveContactFromGroup extends TestBase{
+    @BeforeMethod
+    public void ensurePreconditions(){
+        app.contact().goToHomePage();
+        if (app.db().contacts().size()==0) {
+            app.contact().create(new ContactData()
+                    .withFname("ivan4").withMname("ivanovich").withLname("ivanov").withAddress("123")
+                    .withMobile("999").withEmail("123@ya.ru").withBday("11").withBmonth("January")
+                    .withByear("1990").withAddress2("123123"), true);
+        }
+        if (app.db().groups().size()==0){
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("group1"));
+        }
+
+    }
     @Test
     public void testRemoveContactFromGroup() {
         Contacts beforeContacts = app.db().contacts();
